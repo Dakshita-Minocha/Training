2165f3ff7098aba9447bf2987f736a6e6b3e20b3
Dakshita <dakshitaminocha@metamation.com>
2023-07-25 10:19:50 +0530

    Assignment 2: Guessing Game

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
-      }
+﻿using System;
+
+   int n = new Random ().Next (1, 101);
+   Console.WriteLine ("Guess the number (from 1-100), you have 8 chances: ");
+   int GuessNum = 1;
+   for (; ; ) {
+      Console.Write ($"Guess {GuessNum}: ");
+      GuessNum++;
+      string result = GuessNumber (Console.ReadLine (), n);
+   Console.WriteLine (result);
+   if (result == "Congratulations! You've guessed it right!") break;
+   if (GuessNum > 7) Console.WriteLine ("Too slow! You should've guessed it by now.");      
    }
-}
+
+      string GuessNumber (string Guess, int n) {
+         int.TryParse (Guess, out int Cutoff);
+         if (n==Cutoff) {
+            return "Congratulations! You've guessed it right!";
+         }
+         else if (n < Cutoff) {
+            return "You're too high! Try again";
+         }
+         else {
+            return "You're too low! Try again";
+         }
+      }
+   
