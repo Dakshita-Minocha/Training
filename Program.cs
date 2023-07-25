55a6a8fa946e53ebc93b7aa54208ca252034979d
Dakshita <dakshitaminocha@metamation.com>
2023-07-25 17:46:36 +0530

    Changed the way to calculate number of tries to a more efficient one.

--- Program.cs
+++ Program.cs
@@ -2,16 +2,14 @@
 
    int n = new Random ().Next (1, 101);
    Console.WriteLine ("Guess the number (from 1-100), you have 8 chances: ");
-   int GuessNum = 1;
-   for (; ; ) {
+ 
+   for (int GuessNum = 1; ; GuessNum++) {
       Console.Write ($"Guess {GuessNum}: ");
-      GuessNum++;
       string result = GuessNumber (Console.ReadLine (), n);
    Console.WriteLine (result);
    if (result == "Congratulations! You've guessed it right!") break;
    if (GuessNum > 7) Console.WriteLine ("Too slow! You should've guessed it by now.");      
    }
-
       string GuessNumber (string Guess, int n) {
          int.TryParse (Guess, out int Cutoff);
          if (n==Cutoff) return "Congratulations! You've guessed it right!";
