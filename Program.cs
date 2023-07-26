da7dc2565e987eba3e892e6855c9c07e93a9bb72
Dakshita <dakshitaminocha@metamation.com>
2023-07-26 09:14:39 +0530

    Changes from pull request #1 made. Changed variable names to be more relevant, removed function CheckNumber as it was not required, added comments for describing program functionality.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,17 @@
-﻿using System;
+﻿///This program generates a random number from 1-100 and asks for user input to guess the number. 
+///It generates prompts to help the user guess the number. 
+///It compares the input to the generated number, and prompts the user accordingly. 
+using System;
 
-   int n = new Random ().Next (1, 101);
-   Console.WriteLine ("Guess the number (from 1-100), you have 8 chances: ");
- 
-   for (int GuessNum = 1; ; GuessNum++) {
-      Console.Write ($"Guess {GuessNum}: ");
-      string result = GuessNumber (Console.ReadLine (), n);
-   Console.WriteLine (result);
-   if (result == "Congratulations! You've guessed it right!") break;
-   if (GuessNum > 7) Console.WriteLine ("Too slow! You should've guessed it by now.");      
-   }
-      string GuessNumber (string Guess, int n) {
-         int.TryParse (Guess, out int Cutoff);
-         if (n==Cutoff) return "Congratulations! You've guessed it right!";
-         else if (n < Cutoff) return "You're too high! Try again";
-         else return "You're too low! Try again";
-         
-      }
-   
+int n = new Random ().Next (1, 101);
+Console.WriteLine ("Guess the number (from 1-100), you have 8 chances: ");
+for (int guessnum = 1; ; guessnum++) {
+   if (guessnum > 7) Console.WriteLine ("Too slow! You should've guessed it by now.");
+   Console.Write ($"Guess {guessnum}: ");
+   int.TryParse (Console.ReadLine (), out int cutoff);
+   if (n == cutoff) {
+      Console.WriteLine ("Congratulations! You've guessed it right!");
+      break;
+   } 
+   else Console.WriteLine($"You're too {(n < cutoff ? "high" : "low")}! Try again");
+}
