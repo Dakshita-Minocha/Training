4b20396e1f57f72b67a61d468220cf748385b7ec
Dakshita <dakshitaminocha@metamation.com>
2023-07-27 14:15:57 +0530

    Added a check for keys so that only numbers can be entered. Q or q may be entered to quit.

--- Program.cs
+++ Program.cs
@@ -5,14 +5,18 @@ using System;
 
 int random = new Random ().Next (1, 101);
 Console.WriteLine ("Guess the number (from 1-100).");
-Console.WriteLine ("You should be able to guess it in 7 tries, but don't worry! You can take your time.");
-Console.WriteLine ("You can 'Q' or 'q' to quit anytime.");
+Console.WriteLine ("You should be able to guess it in 7 tries, but don't worry if you can't! You can take your time.");
+Console.WriteLine ("You can press 'Q' or 'q' to quit anytime.");
 for (int count = 1; ; count++) {
-   if (count > 7) Console.WriteLine ("Too slow! You should've guessed it by now.");
+   if (count > 7) Console.WriteLine ("You should've guessed it by now. ");
    Console.Write ($"Guess {count}: ");
-   int.TryParse (Console.ReadLine (), out int guess);
-   if (guess ==0) Environment.Exit (0); 
-   if (random == guess) {
+   string input= Console.ReadLine();
+   if (input is "q" or "Q") Environment.Exit (0);
+   int.TryParse (input, out int guess);
+   if (guess == 0) {
+      Console.WriteLine ("Enter integer from 1-100.");
+      count -= 1;
+   } else if (random == guess) {
       Console.WriteLine ("Congratulations! You've guessed it right!");
       break;
    } else Console.WriteLine ($"You're too {(random < guess ? "high" : "low")}! Try again");
