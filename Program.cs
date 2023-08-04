e1da47e6f518d144c8f38226359109d2daed53bd
Dakshita <dakshitaminocha@metamation.com>
2023-08-04 10:12:11 +0530

    switched order of statements in Filterwords() if condition

--- Program.cs
+++ Program.cs
@@ -13,11 +13,9 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Security.Cryptography;
 
-int i = 0, score = 0;
+int i = 0;
 var scoreCard = new List<(int, string)> ();
 string letters;
-//var letters = new List<char> { 'U', 'X', 'A', 'L', 'T', 'N', 'E' };
-var letterFrequency = new Dictionary<char, long> ();
 // Finding all possible combinations of letters from dictionary:
 string[] words = File.ReadAllLines ("C:\\Users\\dakshitaminocha\\Downloads\\words.txt");
 
@@ -25,7 +23,7 @@ string[] words = File.ReadAllLines ("C:\\Users\\dakshitaminocha\\Downloads\\word
 Console.WriteLine ("Enter 7 letters:");
 letters = Console.ReadLine ().ToUpper ();
 
-// Traversing through array to read characters.
+// Traversing through string to read characters.
 Console.WriteLine ("Your letters:");
 for (i = 0; i < letters.Length; i++) Console.Write ($" {letters[i]} ");
 Console.WriteLine ();
@@ -34,25 +32,21 @@ Console.WriteLine ();
 for (; ; ) {
    Console.WriteLine ("Display using Function1 [1] (uses existing methods), Function2 [2]. Press [Q] to exit.");
    var key = Console.ReadKey (true).Key;
-   Console.WriteLine ();
    switch (key) {
       case ConsoleKey.D1: FilterWords (); break;
-      case ConsoleKey.D2:
-         scoreCard.Clear ();
-         FilterWordsManually (); break;
+      case ConsoleKey.D2: scoreCard.Clear (); FilterWordsManually (); break;
       case ConsoleKey.Q: Environment.Exit (0); break;
-      default: Console.WriteLine ("Enter [1] or [2]"); break;
+      default: Console.WriteLine ("\nEnter [1] or [2]"); break;
    }
 }
 
 void Display (List<(int, string)> scoreCard) {
-   int totalScore = 0;
    var ordered = scoreCard.OrderByDescending (a => a.Item1).ThenBy (a => a.Item2);
    foreach (var (j, val) in ordered) {
-      Console.WriteLine ($"{j,2}. {val,-2}");
+      Console.Write ($"\n{j,2}. {val,-2}");
    }
-   totalScore = scoreCard.Sum (a => a.Item1);
-   Console.WriteLine ("----\n" + totalScore + "  --total");
+   int totalScore = scoreCard.Sum (a => a.Item1);
+   Console.WriteLine ("\n----\n" + totalScore + "  --total");
 }
 
 // Filters through possible combinations with redefined functions like .Contain()
@@ -61,8 +55,8 @@ void Display (List<(int, string)> scoreCard) {
 void FilterWords () {
    foreach (var word in words) {
       if (word.Length >= 4
-      && word.All (letters.Contains)
-      && word.Contains (letters[0].ToString ()) == true)
+      && word.Contains (letters[0].ToString ())
+      && word.All (letters.Contains))
          scoreCard.Add ((ScoreCalculator (word), word));
    }
    Display (scoreCard);
@@ -74,8 +68,8 @@ void FilterWords () {
 void FilterWordsManually () {
    foreach (var word in words) {
       if (word.Length > 3
-      && CheckCharacters (word) != 0
-      && CheckFirstChar (letters[0], word) == 1)
+      && CheckFirstChar (letters[0], word) == 1
+      && CheckCharacters (word) != 0)
          scoreCard.Add ((ScoreCalculator (word), word));
    }
    Display (scoreCard);
@@ -87,13 +81,11 @@ int ScoreCalculator (string word) => (word.Length == 4 ? 1 : word.Length) + (Che
 // Checks if the given word is a Pangram, i.e., uses all 7 letters.
 bool CheckPangram (string word) {
    int count = 0;
-   foreach (var letter in letters) {
-      foreach (var ch in word) {
+   foreach (var letter in letters) 
+      foreach (var ch in word) 
          if (letter == ch) {
             count++; break;
          }
-      }
-   }
    if (count == 7) return true;
    return false;
 }
