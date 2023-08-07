f4942ae54d0815db73c00662fb07757584724b5b
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-07 14:47:43 +0530

    removed padd function

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Training {
             Console.Write ("Enter number or press [Q] to exit: ");
             string input = Console.ReadLine ();
             if (int.TryParse (input, out int number))
-               Console.WriteLine ($"Input: {number}\nHEX: {number:x} \nBinary: {Convert.ToString (number, 2).PadLeft (32, '0')}");
+               Console.WriteLine ($"Input: {number}\nHEX: {number:X} \nBinary: {Convert.ToString (number, 8)}");
             if (input.ToLower() == "q") Environment.Exit (0);
          }
       }
