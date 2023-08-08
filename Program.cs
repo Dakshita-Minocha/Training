502c168c04fdc6e77381f2f06ff9194ad83ee370
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-08 17:04:18 +0530

    Fibonacci Series, initial commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿namespace Training {
    internal class Program {
       static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
+         Console.Write("Enter no of terms to be displayed");
+         int.TryParse (Console.ReadLine(), out int input);
+         int item1 = 0, item2 = 1;
+         Console.Write(item1 + " " + item2 + " ");
+         for (int i = 2; i < input; i++) {
+            int sum = item1 + item2;
+            item1 = item2;
+            item2 = sum;
+            Console.Write (sum + " ");
+         }
       }
    }
 }
