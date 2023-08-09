7c5fee99e922191a0236cef1b85d7c6bafa5fc3f
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-09 13:03:15 +0530

    Check for prime numbers, first commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿namespace Training {
    internal class Program {
+   /// <summary>Program to check if a number is prime</summary>
+   /// <param name="args"></param>
       static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
+      for(; ;){
+            Console.Write ("Enter number: ");
+            int.TryParse (Console.ReadLine (), out var input);
+            Console.WriteLine ($"\n{IsPrime (input)}");
+            Console.Write ("\nDo you want to continue (y/n)?\n");
+            if (Console.ReadKey(true).Key==ConsoleKey.N) Environment.Exit (0);
+         }
+      }
+      static bool IsPrime(int input){
+      for (int i =2; i < input; i++)
+            if (input !=2 && input % i == 0) return false;
+      return true;
       }
    }
 }
