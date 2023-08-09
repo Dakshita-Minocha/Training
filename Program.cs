9b7e255fba1aab3736cf002a070e3d75abcde5a4
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-09 14:54:55 +0530

    Program to check if string is a palindrome.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Training {
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Training {
    internal class Program {
+   /// <summary>Program to check is a string is a palindrome</summary>
+   /// <param name="args"></param>
       static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
+         for(; ; ){
+            Console.Write ("Enter string to check for palindrome: ");
+            string input = Console.ReadLine ().Replace (" ", "").ToLower();
+            Console.Write ($"\n{input} is palindrome? {input == String.Join ("", input.Reverse ())}");
+            Console.WriteLine ("\nDo you want to try again (y/n)?");
+            if (Console.ReadKey (true).Key == ConsoleKey.N) Environment.Exit (0);
+         }
       }
    }
 }
