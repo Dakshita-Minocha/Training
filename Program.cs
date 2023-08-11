e82f18d6a988fc6fe95bb5ad75b71be162b5e55b
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-11 14:26:11 +0530

    Removed redundant lines from code.

--- Program.cs
+++ Program.cs
@@ -2,61 +2,56 @@
 
 namespace Training {
    internal class Program {
-      /// <summary>Program to display Chess Board using Unicode</summary>
-      /// <param name="args"></param>
+      /// <summary>Program to display Chess Board using Unicode.
+      /// The program first prints the grid line (using Printline()), then the line with chess characters as: "| <char> ".
+      /// At the end of the character line it adds another " | " to close grid and print the next grid line.
+      /// It prints the last grid line separately as PrintLine(9) to close grid.</summary>
       static void Main (string[] args) {
-         System.Console.OutputEncoding = new UnicodeEncoding();
+         System.Console.OutputEncoding = new UnicodeEncoding ();
          for (int i = 1; i <= 8; i++) {
             PrintLine (i);
-            Console.WriteLine ();
-            for (int j = 1; j <= 8; j++) {
-               if (j != 9) Console.Write ('\u2502');
-               DisplayChessItem (i, j);
-               Console.Write (" ");
-            }
+            for (int j = 1; j <= 8; j++) 
+                  Console.Write ('\u2502' + $" {ChessItem (i, j)} ");
             Console.Write ('\u2502' + "\n");
-            if (i != 8) Console.Write ('\u251C');
          }
          PrintLine (9);
       }
+
+      /// <summary>Prints 8x8 Grid line by line between character lines</summary>
       static void PrintLine (int row) {
          if (row == 1) Console.Write ('\u250C');
-         if (row == 9) Console.Write ('\u2514');
+         else if (row == 9) Console.Write ('\u2514');
+         else Console.Write ('\u251C');
          for (int i = 1; i <= 8; i++) {
+            Console.Write(new string ('\u2500', 3));
             switch (row) {
-               case 1:
-                  Console.Write ('\u2500'); Console.Write ('\u2500'); Console.Write ('\u2500');
-                  Console.Write (i == 8 ? '\u2510' : '\u252C'); break;
-               case 9:
-                  Console.Write ('\u2500'); Console.Write ('\u2500'); Console.Write ('\u2500');
-                  Console.Write (i == 8 ? '\u2518' : '\u2534'); break;
-               default:
-                  Console.Write ('\u2500'); Console.Write ('\u2500'); Console.Write ('\u2500');
-                  Console.Write (i == 8 ? '\u2524' : '\u253C'); break;
+               case 1: Console.Write (i == 8 ? '\u2510' : '\u252C'); break;
+               case 9: Console.Write (i == 8 ? '\u2518' : '\u2534'); break;
+               default: Console.Write (i == 8 ? '\u2524' : '\u253C'); break;
             }
          }
+         Console.WriteLine ();
       }
 
-      static void DisplayChessItem (int i, int j) {
+      /// <summary>Returns value of character to be printed based on position in 8x8 grid</summary>
+      /// <param name="i">row</param>
+      /// <param name="j">column</param>
+      static char ChessItem (int i, int j) {
          switch (i) {
-            case 1:
-               if (j is 1 or 8) Console.Write (" " + '\u2656');
-               else if (j is 3 or 6) Console.Write (" " + '\u2657');
-               else if (j is 2 or 7) Console.Write (" " + '\u2658');
-               else if (j == 4) Console.Write (" " + '\u2655');
-               else if (j == 5) Console.Write (" " + '\u2654');
-               break;
-            case 2: Console.Write (" " + '\u2659'); break;
-            case 7: Console.Write (" " + '\u265F'); break;
-            case 8:
-               if (j is 1 or 8) Console.Write (" " + '\u265C');
-               else if (j is 3 or 6) Console.Write (" " + '\u265D');
-               else if (j is 2 or 7) Console.Write (" " + '\u265E');
-               else if (j == 4) Console.Write (" " + '\u265B');
-               else if (j == 5) Console.Write (" " + '\u265A');
-               break;
-            default: Console.Write (" " + " "); break;
+            case 1:  // white pieces
+               if (j is 1 or 8) return '\u2656';
+               else if (j is 3 or 6) return '\u2657';
+               else if (j is 2 or 7) return '\u2658';
+               return j == 4 ? '\u2655' : '\u2654';
+            case 2: return '\u2659'; // white pawns
+            case 7: return '\u265F'; // black pawns
+            case 8: // black pieces
+               if (j is 1 or 8) return '\u265C';
+               else if (j is 3 or 6) return '\u265D';
+               else if (j is 2 or 7) return '\u265E';
+               return j == 4 ? '\u265B' : '\u265A';
          }
+         return ' ';
       }
    }
 }
