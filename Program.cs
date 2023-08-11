90c4d0d4702fd5eaca787bcccbf26f12706b2e40
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-11 15:42:26 +0530

    Calculating LCM and HCF for n numbers.

--- Program.cs
+++ Program.cs
@@ -2,15 +2,29 @@
 
 namespace Training {
    internal class Program {
-      /// <summary>Program to generate LCM and HCF of 2 numbers</summary>
+      /// <summary>Program to generate LCM and HCF of any n numbers</summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
-         Console.WriteLine ("Enter two numbers: ");
-         int.TryParse (Console.ReadLine (), out int num1);
-         int.TryParse (Console.ReadLine (), out int num2);
-         if (num1 > num2) (num1, num2) = (num2, num1);
-         Console.Write ($"\nLCM: {LCM (num1, num2)}\n");
-         Console.Write ($"HCF: {HCF (num1, num2)}\n");
+         Console.WriteLine ("Enter no of parameters: ");
+         int.TryParse (Console.ReadLine (), out int count);
+         Console.WriteLine ($"Enter {count} numbers: ");
+         int num1;
+         var num = new List <int>();
+         for (int i = 0;i < count; i++) {
+            int.TryParse (Console.ReadLine (), out num1);
+            num.Add (Math.Abs(num1));
+            }
+         num.Sort ();
+         Console.Write ($"\nLCM: { Calc(num).Item1}\n");
+         Console.Write ($"HCF: {Calc (num).Item2}\n");
+      }
+      static (int, int) Calc(List<int> num){
+         int lcm = num[0], hcf =num[num.Count()-1];
+         for (int i=1; i<num.Count(); i++) {
+           lcm = LCM(num[i], lcm);
+           hcf = HCF(num[i], hcf);
+         }
+         return (lcm, hcf);
       }
       static int LCM (int num1, int num2) {
          for (int i = num2; i < num1 * num2; i++)
