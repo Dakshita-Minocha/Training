b458a08b6798cce26d4523d52f1f434ff6386a87
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-14 09:34:04 +0530

    Changes as requested in pull request 10

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
+﻿using System.Text.RegularExpressions;
 namespace Training {
    internal class Program {
-   /// <summary>Program to check is a string is a palindrome</summary>
-   /// <param name="args"></param>
-      static void Main(string[] args) {
-         for(; ; ){
+      /// <summary>Program to check is a string is a palindrome</summary>
+      /// <param name="args"></param>
+      static void Main (string[] args) {
+         for (; ; ) {
             Console.Write ("Enter string to check for palindrome: ");
-            string input = Console.ReadLine ().Replace (" ", "").ToLower();
-            Console.Write ($"\n{input} is palindrome? {input == String.Join ("", input.Reverse ())}");
-            Console.WriteLine ("\nDo you want to try again (y/n)?");
-            if (Console.ReadKey (true).Key == ConsoleKey.N) Environment.Exit (0);
+            string input = Console.ReadLine ();
+            string str = Regex.Replace (input, @"[^a-zA-Z]+", "").ToLower ();
+            Console.Write (str == String.Join ("", str.Reverse ()) ? $"\n\"{input}\" is a palindrome\n" : $"\n\"{input}\" is not a palindrome\n");
+            for (; ; ) {
+               Console.WriteLine ("\nDo you want to try again (y/n)?");
+               var key = Console.ReadKey (true).Key;
+               if (key == ConsoleKey.N) Environment.Exit (0);
+               else if (key == ConsoleKey.Y) break;
+               else Console.WriteLine ("Enter [y] or [n].");
+            }
          }
       }
    }
