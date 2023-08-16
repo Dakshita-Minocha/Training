174292017ad22cc02b84247636f68e91eac190ad
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-16 14:58:19 +0530

    Maths table: Initial commit.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿namespace Training {
    internal class Program {
       static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
+      for(; ;){
+            Console.Write ("Enter number: ");
+            int.TryParse (Console.ReadLine (), out int number);
+            if (number == 0) Console.Write (" 0 * any number is Zero.");
+            else{
+               for (int i = 1; i <= 10; i++)
+                  Console.Write ($"{number} * {i,2} = {number * i} \n");
+            }
+            Console.Write ("\nPress 'q' to exit, any other key to continue\n");
+            if (Console.ReadKey(true).Key == ConsoleKey.Q) Environment.Exit (0);
+            Console.WriteLine ();
+         }
       }
    }
 }
