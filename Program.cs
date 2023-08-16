d782c1d16fbc3b0801670fc610883d9f3a62cb12
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-16 14:22:45 +0530

    Changes as per PR #4

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ for (; ; ) {
       bool flag = true;
       Console.WriteLine ("Enter letters:");
       letters = Console.ReadLine ().ToUpper ();
-      letters = letters.Substring (0, 7);
+      letters = letters[0..7];
       foreach (var ch in letters) if (!char.IsLetter (ch)) flag = false;
       if (flag) break;
    }
@@ -30,12 +30,11 @@ for (; ; ) {
 }
 
 // Output Display: 
-void Display (List<(int, string)> scoreCard) {
-   var ordered = scoreCard.OrderByDescending (a => a.Item1).ThenBy (a => a.Item2);
+void Display (List<(int score, string word)> scoreCard) {
+   var ordered = scoreCard.OrderByDescending (a => a.score).ThenBy (a => a.word);
    foreach (var (j, val) in ordered)
       Console.Write ($"\n{j,2}. {val,-2}");
-   int totalScore = scoreCard.Sum (a => a.Item1);
-   Console.WriteLine ("\n----\n" + totalScore + "  total");
+   Console.WriteLine ("\n----\n" + scoreCard.Sum (a => a.score) + "  total");
    scoreCard.Clear ();
 }
 
@@ -45,7 +44,7 @@ void Display (List<(int, string)> scoreCard) {
 void FilterWords () {
    foreach (var word in words)
       if (word.Length >= 4
-      && word.Contains (letters[0].ToString ())
+      && word.Contains (letters[0])
       && word.All (letters.Contains))
          scoreCard.Add ((ScoreCalculator (word), word));
    Display (scoreCard);
