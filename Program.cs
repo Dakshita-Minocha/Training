ef0b85436e40e60e7900970e650d549603988710
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-17 09:21:34 +0530

    Changes made according to pull request #8 Changed variable names, added space to display string, formatted.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿namespace Training {
    internal class Program {
-      static void Main(string[] args) {
-         Console.Write("Enter no of terms to be displayed");
-         int.TryParse (Console.ReadLine(), out int input);
-         int item1 = 0, item2 = 1;
-         Console.Write(item1 + " " + item2 + " ");
+      static void Main (string[] args) {
+         Console.Write ("Enter no of terms to be displayed: ");
+         int.TryParse (Console.ReadLine (), out int input);
+         int num1 = 0, num2 = 1;
+         Console.Write (num1 + " " + num2 + " ");
          for (int i = 2; i < input; i++) {
-            int sum = item1 + item2;
-            item1 = item2;
-            item2 = sum;
+            int sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
             Console.Write (sum + " ");
          }
       }
