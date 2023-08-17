a4cc8a20301b3bf2024110ccc48b07efa98e28b7
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-17 09:50:01 +0530

    T09: commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Training {
          int i, j, count = 1;
          for (i = 0; i < rows; i++) {
             for (j = rows; j > 0; j--)
-               if (i < j) Console.Write ("-");
+               if (i < j) Console.Write (" ");
             for (j = count; j > 0; j--)
                Console.Write ("*");
             count += 2;
@@ -18,10 +18,10 @@ namespace Training {
          }
          count = 1;
          for (i = 1; i <= rows; i++) {
-            for (j = 1; j <= count; j++)
+            for (j = 1; j <= count + 1; j++)
                Console.Write (" ");
             count++;
-            for (j = 1; j <= 2 * (rows - i) - 1; j++)
+            for (j = 1; j < 2 * (rows - i); j++)
                Console.Write ("*");
             Console.WriteLine ();
          }
