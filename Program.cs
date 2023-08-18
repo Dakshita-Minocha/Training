ac11bc0eaa055bc4b9d3cfb719c162bf0d6a9884
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-18 14:52:11 +0530

    Changes as per PR #10

--- Program.cs
+++ Program.cs
@@ -1,21 +1,16 @@
-﻿using System.Text.RegularExpressions;
-namespace Training {
+﻿namespace Training {
    internal class Program {
       /// <summary>Program to check is a string is a palindrome</summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
          for (; ; ) {
-            Console.Write ("Enter string to check for palindrome: ");
+            string str = "";
+            Console.Write ("Enter word or phrase, press [Q] to exit: ");
             string input = Console.ReadLine ();
-            string str = Regex.Replace (input, @"[^a-zA-Z]+", "").ToLower ();
-            Console.Write (str == String.Join ("", str.Reverse ()) ? $"\n\"{input}\" is a palindrome\n" : $"\n\"{input}\" is not a palindrome\n");
-            for (; ; ) {
-               Console.WriteLine ("\nDo you want to try again (y/n)?");
-               var key = Console.ReadKey (true).Key;
-               if (key == ConsoleKey.N) Environment.Exit (0);
-               else if (key == ConsoleKey.Y) break;
-               else Console.WriteLine ("Enter [y] or [n].");
-            }
+            if (input.ToLower() == "q") Environment.Exit (0);
+            foreach (var ch in input .Where(ch=> Char.IsAsciiLetter(ch)))
+                  str += Char.ToLower (ch);
+            Console.Write ($"\n\n{input} is " + (str == String.Join ("", str.Reverse ()) ? "" : "NOT ") + "a palindrome.\n");
          }
       }
    }
