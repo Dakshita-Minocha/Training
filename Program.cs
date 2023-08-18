5aa9afec8ef1f0b02a733c7632d8a6525dc13c0e
Dakshita Minocha <dakshitaminocha@metamation.com>
2023-08-18 12:58:59 +0530

    Removed Func FrequencyCalculator(), PR #5

--- Program.cs
+++ Program.cs
@@ -8,26 +8,18 @@ namespace Training {
       static void Main (string[] args) {
          string[] words = File.ReadAllLines ("C:/etc/words.txt");
          var letterFrequency = new Dictionary<char, int> ();
-         letterFrequency = FrequencyCalculator (words);
-         Console.Write ("Frequency Table:\n" + "Keys  Values\n");
-         foreach (var a1 in letterFrequency.OrderByDescending (a => a.Value))
+         foreach (var word in words)
+         foreach (var letter in word .Where(letter => Char.ToUpper(letter) is >='A' and <='Z')){
+               letterFrequency.TryGetValue (letter, out int freq);
+               letterFrequency[letter] = freq + 1;
+         }
+         var ordered = letterFrequency.OrderByDescending (a => a.Value).ToDictionary(kvp=>kvp.Key, kvp=>kvp.Value);
+         Console.Write ("Frequency Table:\n\nLetter  Freq\n------  ----\n");
+         foreach (var a1 in ordered)
             Console.WriteLine ($"{a1.Key,4} : {a1.Value,5}");
          Console.Write ("7 most frequently appearing characters: ");
-         foreach (var kvp in letterFrequency.OrderByDescending (kvp => kvp.Value).Take (7))
+         foreach (var kvp in ordered.Take (7))
             Console.Write (kvp.Key + " ");
-      }
-      /// <summary>Calculates the frequency of the letters occurring in the string[] words and displays the 7 most frequently occuring letters.</summary>
-      /// <param name="words">Contains the words from the dictionary file.</param>
-      static Dictionary<char, int> FrequencyCalculator (string[] words) {
-         var letterFrequency = new Dictionary<char, int> ();
-         foreach (var word in words)
-            foreach (var letter in word
-                                  .Where (letter => Char.ToUpper (letter) is >= 'A' and <= 'Z')) {
-               if (letterFrequency.ContainsKey (letter))
-                  letterFrequency[letter] = ++letterFrequency[letter];
-               else letterFrequency.Add (letter, 1);
-            }
-         return letterFrequency;
       }
    }
 }
