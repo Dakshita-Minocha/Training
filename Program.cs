9c729fb479c6b8f0e0febb4cf30cbd3195ebe62a
Dakshita <DakshitaMinocha@metamation.com>
2023-08-21 11:03:58 +0530

    Changed loop exit condition

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
 ﻿namespace Training {
    internal class Program {
-      static void Main(string[] args) {
-      for(; ;){
-            Console.Write ("Enter number: ");
-            int.TryParse (Console.ReadLine (), out int number);
+      static void Main (string[] args) {
+         for (; ; ) {
+            Console.Write ("Enter number, press [Q] to exit: ");
+            string input = Console.ReadLine ();
+            if (input.ToLower () == "q") Environment.Exit (0);
+            int.TryParse (input, out int number);
             if (number == 0) Console.Write (" 0 * any number is Zero.");
-            else{
+            else {
                for (int i = 1; i <= 10; i++)
                   Console.Write ($"{number} * {i,2} = {number * i} \n");
             }
-            Console.Write ("\nPress 'q' to exit, any other key to continue\n");
-            if (Console.ReadKey(true).Key == ConsoleKey.Q) Environment.Exit (0);
-            Console.WriteLine ();
          }
       }
    }
