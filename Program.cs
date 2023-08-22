6194eb6d19a1654686b944e75a9e1af21727d747
Dakshita <DakshitaMinocha@metamation.com>
2023-08-22 11:06:09 +0530

    using strings instead for nested for loop for upper diamond.

--- Program.cs
+++ Program.cs
@@ -10,13 +10,10 @@ namespace Training {
             string row = Console.ReadLine ();
             if (row.ToLower () == "q") Environment.Exit (0);
             int.TryParse (row, out int rows);
+            int i, j = rows, scount;
             // Printing upper half of diamond: j ' ' followed by scount '*'.
-            int i, j, scount = 1;
-            for (i = 0; i <= rows; i++, scount += 2) {
-               for (j = 0; j <= rows; j++)
-                  Console.Write (i < j ? " " : "");
-               Console.Write (new string ('*', scount) + '\n');
-            }
+            for (i = 0, scount = 1; i <= rows; i++, scount += 2)
+               Console.Write (new string (' ', j--) + new string ('*', scount) + '\n');
             // Printing lower half of diamond: i ' ' followed by scount '*'.
             for (i = 1, scount = rows * 2 - 1; i <= rows; i++, scount -= 2)
                Console.Write (new string (' ', i) + new string ('*', scount) + '\n');
