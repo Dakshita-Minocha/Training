076e520d5159ec3dd651759b1c5999f3594aa016
Dakshita <DakshitaMinocha@metamation.com>
2023-08-24 09:28:48 +0530

    Formatting code.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿namespace Training {
    internal class Program {
-   /// <summary>Program to check if a number is prime</summary>
-      static void Main(string[] args) {
-      for(; ;){
+      /// <summary> Program to check if a number is prime </summary>
+      static void Main (string[] args) {
+         for (; ; ) {
             Console.Write ("Enter number, press [Q] to exit: ");
-            string input = Console.ReadLine();
-            if (input.ToLower() == "q") Environment.Exit(0);
+            string input = Console.ReadLine ();
+            if (input.ToLower () == "q") Environment.Exit (0);
             int.TryParse (input, out int num);
-            Console.WriteLine ($"{num} is " + (IsPrime(num) ? "" : "NOT ") + "prime");
+            Console.WriteLine ($"{num} is " + (IsPrime (num) ? "" : "NOT ") + "prime");
          }
       }
-      static bool IsPrime(int input){
-      for (int i =2; i <= Math.Sqrt(input); i++)
-            if (input !=2 && input % i == 0) return false;
-      return true;
+      static bool IsPrime (int input) {
+         for (int i = 2; i <= Math.Sqrt (input); i++)
+            if (input != 2 && input % i == 0) return false;
+         return true;
       }
    }
 }
