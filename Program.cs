bfa11268eed918c7c62c213d4b6b049f2c3433f3
Dakshita <DakshitaMinocha@metamation.com>
2023-08-24 17:49:41 +0530

    Password check: Initial commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿namespace Training {
    internal class Program {
-      static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
+      /// <summary> Program to determine whether entered assword is strong </summary>
+      static void Main () {
+         Console.WriteLine ("Enter password: ");
+         string input = Console.ReadLine ();
+         string reason = CheckPassword (input);
+         Console.WriteLine ((reason == "" ? "C" : "Inc") + "orrect Password\n" + reason);
+      }
+      static string CheckPassword (string password) {
+         string reason = "", special = "!@#$%^&*()-+", str = "Password needs to have atleast one ";
+         if (password.Length < 6) reason += "Password length needs to be greater than 6.\n";
+         if (password.All (c => !char.IsUpper (c))) reason += str + "upper case character.\n";
+         if (password.All (c => !char.IsLower (c))) reason += str + "lower case character.\n";
+         if (password.All (c => !char.IsDigit (c))) reason += str + "digit.\n";
+         if (password.All (c => !special.Contains (c))) reason += str + "special character [!@#$%^&*()-+].\n";
+         return reason;
       }
    }
 }
