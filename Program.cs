52348f70265deaafabbbf1b6eb0edad0d9ad50b9
Dakshita <DakshitaMinocha@metamation.com>
2023-08-28 16:02:02 +0530

    Isogram: Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿namespace Training {
    internal class Program {
-      static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
+      /// <summary> Program to check if a word is an isogram </summary>
+      static void Main () {
+         for (; ; ) {
+            Console.Write ("Enter word or press [Q] to exit: ");
+            string input = Console.ReadLine (), str;
+            if (input.ToLower () == "q") Environment.Exit (0);
+            bool flag = true;
+            int i = 0, len = input.Length;
+            while (i < len) {
+               str = input[(i + 1)..len];
+               if (str.Any (c => c == input[i])) {
+                  flag = false; break;
+               }
+               i++;
+            }
+            Console.WriteLine ($"{input} is " + (flag ? "" : "NOT ") + "an Isogram.");
+         }
       }
    }
 }
