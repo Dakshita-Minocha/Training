4ea64e8b5e814098bb19f3ad06b6af97ac45c150
Dakshita <DakshitaMinocha@metamation.com>
2023-08-28 15:25:31 +0530

    Reduced string: initial commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿namespace Training {
    internal class Program {
-      static void Main(string[] args) {
-         Console.WriteLine("Hello, World!");
+      /// <summary> Program to reduce a string of lowercase characters by deleting a pair of adjacent letters that match. </summary>
+      static void Main () {
+         for (; ; ) {
+            Console.WriteLine ("Enter String or press [Q] to exit: ");
+            string input = Console.ReadLine ().ToLower ();
+            if (input == "q") Environment.Exit (0);
+            int i = 0;
+            while (i < input.Length - 1) {
+               if (input[i] == input[i + 1]) input = input.Remove (i, 2);
+               else i++;
+            }
+            Console.WriteLine ($"Reduced string: {input}");
+         }
       }
    }
 }
