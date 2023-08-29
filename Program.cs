303c71ce53c4734f2e4a099558a98f9e56a20b9c
Dakshita <DakshitaMinocha@metamation.com>
2023-08-29 14:54:36 +0530

    Adding check for 0 and letters other than q

--- Program.cs
+++ Program.cs
@@ -2,12 +2,17 @@
    internal class Program {
       /// <summary> Program to check if a number is prime </summary>
       static void Main (string[] args) {
+         string input;
          for (; ; ) {
-            Console.Write ("Enter number, press [Q] to exit: ");
-            string input = Console.ReadLine ();
-            if (input.ToLower () == "q") Environment.Exit (0);
+            for (; ; ) {
+               Console.Write ("Enter number, press [Q] to exit: ");
+               input = Console.ReadLine ();
+               if (input.ToLower () == "q") Environment.Exit (0);
+               if (input.All (c => Char.IsDigit (c))) break;
+            }
             int.TryParse (input, out int num);
-            Console.WriteLine ($"{num} is " + (IsPrime (num) ? "" : "NOT ") + "prime");
+            if (num == 0) Console.WriteLine ("0 is NOT prime");
+            else Console.WriteLine ($"{num} is " + (IsPrime (num) ? "" : "NOT ") + "prime");
          }
       }
       static bool IsPrime (int input) {
