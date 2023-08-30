7b478eea60bf6eeab60b09ca83274f0a1448b8b3
Dakshita <DakshitaMinocha@metamation.com>
2023-08-30 12:03:15 +0530

    Changes made as per pull request #5

--- Program.cs
+++ Program.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Diagnostics.Metrics;
-using System.IO;
-using System.Linq;
 namespace Training {
    internal class Program {
       /// <summary> Program to read through a file and find the frequency of letters occurring in it and store it in a dictionary. </summary>
-      static void Main (string[] args) {
+      static void Main () {
          string[] words = File.ReadAllLines ("C:/etc/words.txt");
+         var list = new List<string> ();
          var letterFrequency = new Dictionary<char, int> ();
-         foreach (var word in words)
-         foreach (var letter in word .Where(letter => Char.ToUpper(letter) is >='A' and <='Z')){
+         foreach (var word in words) {
+            foreach (var letter in word.Where (letter => Char.ToUpper (letter) is >= 'A' and <= 'Z')) {
                letterFrequency.TryGetValue (letter, out int freq);
                letterFrequency[letter] = freq + 1;
+            }
          }
-         var ordered = letterFrequency.OrderByDescending (a => a.Value).ToDictionary(kvp=>kvp.Key, kvp=>kvp.Value);
+         var ordered = letterFrequency.OrderByDescending (a => a.Value);
          Console.Write ("Frequency Table:\n\nLetter  Freq\n------  ----\n");
          foreach (var a1 in ordered)
             Console.WriteLine ($"{a1.Key,4} : {a1.Value,5}");
          Console.Write ("7 most frequently appearing characters: ");
          foreach (var kvp in ordered.Take (7))
             Console.Write (kvp.Key + " ");
+
       }
    }
 }
