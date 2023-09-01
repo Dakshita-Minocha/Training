874242613f7e81d1518e51700717831cc0959fb0
Dakshita <DakshitaMinocha@metamation.com>
2023-09-01 16:43:24 +0530

    Switching to command line arguments

--- Program.cs
+++ Program.cs
@@ -1,35 +1,22 @@
 namespace Training {
    internal class Program {
       /// <summary> Program to check if a number is an Armstrong number </summary>
-      static void Main () {
+      static void Main (string[] args) {
          string input;
-         int n;
-         for (; ; ) {
-            Console.WriteLine ("Choose from menu or press [Q] to exit: \n" +
-                               "[1]Check if number is Armstrong\n" +
-                               "[2]Find nth Armstrong number [n<=25]");
-            var ch = Console.ReadKey (true).Key;
-            switch (ch) {
-               case ConsoleKey.D1:
-                  for (; ; ) {
-                     Console.Write ("Enter number: ");
-                     input = Console.ReadLine ();
-                     if (input.All (c => Char.IsAsciiDigit (c))) break;
-                  }
-                  int.TryParse (input, out int num);
-                  Console.WriteLine ($"{num} is " + (Armstrong (num) ? "" : "NOT ") + "an Armstrong number.\n"); break;
-               case ConsoleKey.D2:
-                  for (; ; ) {
-                     Console.WriteLine ("Enter nth number: ");
-                     input = Console.ReadLine ();
-                     int.TryParse (input, out n);
-                     if (n != 0 & n <= 25) break;
-                  }
-                  Console.WriteLine ($"{n}th Armstrong number = {nArmstrong (n)}\n"); break;
-               case ConsoleKey.Q: Environment.Exit (0); break;
+         if (args.Length == 0) {
+            Console.Write ("Enter number: ");
+            input = Console.ReadLine ();
+            if (input.All (c => Char.IsAsciiDigit (c))) {
+               int.TryParse (input, out int num);
+               Console.WriteLine ($"{num} is " + (Armstrong (num) ? "" : "NOT ") + "an Armstrong number.");
             }
+         } else {
+            input = args[0];
+            int.TryParse (input, out int n);
+            if (n != 0 & n <= 25) Console.WriteLine ($"{n}th Armstrong number = {nArmstrong (n)}");
          }
       }
+
       static int nArmstrong (int n) {
          int count = 1, num = 1;
          while (count < n) {
