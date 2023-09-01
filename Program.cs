2684ee767df2af65348f0641a8ad95ce3d1cbde2
Dakshita <DakshitaMinocha@metamation.com>
2023-09-01 17:49:59 +0530

    using ToLower() for uppercase char cases.

--- Program.cs
+++ Program.cs
@@ -2,18 +2,24 @@
    internal class Program {
       /// <summary> Longest abecedarian word in an array </summary>
       static void Main () {
-         string[] words = new[] { "Abstemious", "sup", "Bijoux", "never", "Arsenic", "Chintz", "Facetious", "Aegilops" };
+         string[] words = new[] { "Abstemious", "sup", "Bijoux", "never", "ArENnic", "Chintz", "Facetious", "AeGIlops" };
+         var wcopy = words.ToArray ();
+         int i;
          Dictionary<string, int> isograms = new ();
-         foreach (var word in words) {
+         foreach (var word in wcopy.Select (w => w.ToLower ())) {
             bool flag = true;
-            for (int i = 0; i < word.Length - 1; i++)
+            for (i = 0; i < word.Length - 1; i++)
                if (word[i] > word[i + 1]) {
                   flag = false; break;
                }
-            if (flag) isograms.Add (new string (word), word.Length);
+            if (flag) {
+               isograms.TryGetValue (new string (words[i]), out int len);
+               isograms[words[i]] = words[i].Length;
+            }
          }
          var kvp = isograms.OrderByDescending (a => a.Value).FirstOrDefault ();
          Console.WriteLine ($"Largest abecedarian word is {kvp.Key}");
+         isograms.Clear ();
       }
    }
 }
