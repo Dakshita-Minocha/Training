07805f6810fda587240674e925dc0e32d23f0a72
Dakshita <DakshitaMinocha@metamation.com>
2023-09-01 09:41:26 +0530

    Changes made according to PR#12

--- Program.cs
+++ Program.cs
@@ -2,17 +2,24 @@ namespace Training {
    internal class Program {
       /// <summary>Program to generate LCM and HCF of any n numbers</summary>
       static void Main () {
-         Console.WriteLine ("How many numbers would you like to enter? ");
-         int.TryParse (Console.ReadLine (), out int count);
-         Console.WriteLine ($"Enter {count} numbers: ");
-         var num = new List<int> ();
-         for (int i = 0; i < count; i++) {
-            int.TryParse (Console.ReadLine (), out int num1);
-            num.Add (Math.Abs (num1));
+         string input;
+         for (; ; ) {
+            Console.WriteLine ("How many numbers would you like to enter? Enter [Q] to exit: ");
+            input = Console.ReadLine ();
+            if (input.ToLower () == "q") Environment.Exit (0);
+            int.TryParse (input, out int count);
+            if (count > 0) {
+               Console.WriteLine ($"Enter {count} numbers: ");
+               var num = new List<int> ();
+               for (int i = 0; i < count; i++) {
+                  int.TryParse (Console.ReadLine (), out int num1);
+                  num.Add (Math.Abs (num1));
+               }
+               num.Sort ();
+               Console.Write ($"\nLCM: {Calc (num).lcm}\n");
+               Console.Write ($"HCF: {Calc (num).hcf}\n");
+            }
          }
-         num.Sort ();
-         Console.Write ($"\nLCM: {Calc (num).lcm}\n");
-         Console.Write ($"HCF: {Calc (num).hcf}\n");
       }
       /// <summary> Passes n numbers to LCM () and HCF () functions and returns lcm and hcf values. 
       ///           Sets lcm = hcf = 0 if any of entered values = 0. </summary>
@@ -20,8 +27,8 @@ namespace Training {
       /// <returns>(lcm, hcf)</returns>
       static (int lcm, int hcf) Calc (List<int> num) {
          int lcm = num[0], hcf = num[0];
+         if (num.Any (d => d == 0)) return (0, 0);
          for (int i = 1; i < num.Count; i++) {
-            if (num[i - 1] == 0) return (0, 0);
             lcm = LCM (lcm, num[i]);
             hcf = HCF (num[i], hcf);
          }
