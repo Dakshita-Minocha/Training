92189ad9b9007037a01aced5f6af45f39eb4128a
Dakshita <DakshitaMinocha@metamation.com>
2023-09-01 10:22:35 +0530

    Changes made as per pull request #21

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@
       /// <summary> Longest abecedarian word in an array </summary>
       static void Main () {
          string[] words = new[] { "Abstemious", "sup", "Bijoux", "never", "Arsenic", "Chintz", "Facetious", "Aegilops" };
-         var isograms = new Dictionary<string, int> ();
-         foreach (var word in words.Select (word => word.ToLower ().ToCharArray ())) {
+         Dictionary<string, int> isograms = new ();
+         foreach (var word in words) {
             bool flag = true;
             for (int i = 0; i < word.Length - 1; i++)
                if (word[i] > word[i + 1]) {
@@ -12,8 +12,8 @@
                }
             if (flag) isograms.Add (new string (word), word.Length);
          }
-         foreach (var kvp in isograms.OrderByDescending (a => a.Value).Take (1))
-            Console.WriteLine ($"Largest abecedarian word is {kvp.Key}");
+         var kvp = isograms.OrderByDescending (a => a.Value).FirstOrDefault ();
+         Console.WriteLine ($"Largest abecedarian word is {kvp.Key}");
       }
    }
 }
