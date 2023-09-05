4f54e8af1d7db0cf9d08cf29bd8d4ea7b0a267f3
Dakshita <DakshitaMinocha@metamation.com>
2023-09-05 14:41:06 +0530

    Changes as per PR#25

--- Program.cs
+++ Program.cs
@@ -2,33 +2,37 @@
    internal class Program {
       /// <summary> Program to display the individual digits of a given number </summary>
       static void Main () {
-         List<int> digits = new ();
+         CallDigits ();
+      }
+      static void CallDigits () {
          for (; ; ) {
             Console.Write ("Enter number, or enter [Q] to exit: ");
             string input = Console.ReadLine ();
             if (input.ToLower () == "q") Environment.Exit (0);
-            if (input != "" & // not empty
-               (char.IsDigit (input[0]) | input[0] == '-') & // Check if input is negative
-                input[1..input.Length].All (char.IsDigit)) {
-               int.TryParse (input, out int num);
-               Digits (Math.Abs (num), digits);
-               Console.Write ($"Digits of {num}: ");
-               digits.Reverse ();
-               foreach (int digit in digits)
-                  Console.Write ($"{digit} ");
+            var strArray = input.Split (".");
+            if (double.TryParse (input, out double n)) {
+               Console.WriteLine ($"Digits of {input}: ");
+               foreach (string item in strArray) {
+                  int.TryParse (item, out int num);
+                  var listDigits = GetDigits (Math.Abs (num)).ToList ();
+                  Console.WriteLine ((Array.IndexOf (strArray, item) == 0 ? "Integer part: " : "Decimal Part: ") +
+                                      string.Join (" ", listDigits));
+               }
             }
-            digits.Clear ();
-            Console.WriteLine ();
          }
       }
       /// <summary> Adds digits of number to list in reverse order </summary>
       /// <param name="num"> User input </param>
       /// <param name="digits"> List of individual digits </param>
-      static void Digits (int num, List<int> digits) {
+      /// <returns> List of digits </returns>
+      static List<int> GetDigits (int num) {
+         List<int> digits = new ();
          while (num > 0) {
             digits.Add (num % 10);
             num /= 10;
          }
+         digits.Reverse ();
+         return digits;
       }
    }
 }
