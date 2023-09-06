ee5a191537f38af7319460cd5fc9a363259a64a8
Dakshita <DakshitaMinocha@metamation.com>
2023-09-06 09:59:01 +0530

    Changing program to accept two numbers and swap their values.

--- Program.cs
+++ Program.cs
@@ -2,27 +2,20 @@
    internal class Program {
       /// <summary> Program to swap a number </summary>
       static void Main () {
-         int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-         foreach (int e in arr) Console.Write ($"{e} ");
          for (; ; ) {
-            Console.WriteLine ("\nEnter 2 numbers from given numbers to swap or [Q] to exit.");
+            Console.WriteLine ("\nEnter 2 numbers (\"a b\") to swap, or [Q] to exit.");
             string input = Console.ReadLine ();
             if (input.ToLower () == "q") Environment.Exit (0);
-            int.TryParse (input, out int num1);
-            input = Console.ReadLine ();
-            int.TryParse (input, out int num2);
-            if (arr.Contains (num1) & arr.Contains (num2)) {
-               Swap (arr, Array.IndexOf (arr, num1), Array.IndexOf (arr, num2));
-               Console.WriteLine ("Numbers after swapping: ");
-               foreach (int e in arr) {
-                  if (e == num1 | e == num2) Console.ForegroundColor = ConsoleColor.Yellow;
-                  Console.Write ($"{e} ");
-                  Console.ResetColor ();
+            string[] num = input.Split ();
+            if (num.Length < 3) {
+               if (int.TryParse (num[0], out int num1) && int.TryParse (num[1], out int num2)) {
+                  Console.WriteLine ($"Numbers:\na = {num1}\nb = {num2}");
+                  (num1, num2) = Swap (num1, num2);
+                  Console.WriteLine ($"Numbers after swapping:\na = {num1}\nb = {num2}");
                }
             }
-            Console.WriteLine ();
          }
+         static (int, int) Swap (int num1, int num2) => (num2, num1);
       }
-      static void Swap (int[] arr, int i1, int i2) => (arr[i1], arr[i2]) = (arr[i2], arr[i1]);
    }
 }
