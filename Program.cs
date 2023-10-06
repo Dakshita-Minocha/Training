9e4958d8234b65401e73eafb7f77b4716e101f76
Dakshita <dakshitaminocha@metamation.com>
2023-10-06 16:38:38 +0530

    Made changes to LSB-MSB logic so it works

--- Program.cs
+++ Program.cs
@@ -3,16 +3,59 @@
 // Copyright (c) Metamation India.                                                
 // ---------------------------------------------------------------------------
 // Program.cs                                                                     
-// Program to swap indices from predefined array.
-// User enters two indices to swap and new array with swapped elements is displayed. 
-// Swapped elements and their indices are displayed in Green Foreground Color.
+// Program to find the number thought of by the user with two methods: MSB-LSB or LSB-MSB
 // ---------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training {
    #region class Program ------------------------------------------------------------------------
    internal class Program {
       #region Method ----------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+      static int high = 101, low = 1, bGuess = high / 2;
+      static void Main (string[] args) => Menu ();
+
+      static void Menu () {
+         WriteLine ("Would you like to guess with Binary Method from MSB-LSB(M) or in Reverse Order from LSB-MSB(L)?\nPress (Q) to quit.");
+         var key = ReadKey (true).Key;
+         switch (key) {
+            case ConsoleKey.M: GuessNumberBinaryMSB_LSB (); break;
+            case ConsoleKey.L: GuessNumberLSB_MSB (); break;
+            case ConsoleKey.Q: Environment.Exit (0); break;
+            default:
+               Menu (); break;
+         }
+      }
+
+      /// <summary> CheckYes if the value guessed is correct and exit program if it is. </summary>
+      /// <param name="prompt"> User Input </param>
+      static bool CheckYes (string prompt) {
+         if (prompt.ToLower () == "yes") {
+            Write ("\nYay! We guessed it!\nDo you want to exit(y/n)? \n");
+            if (ReadKey (true).Key == ConsoleKey.Y)
+               Environment.Exit (0);
+            Menu ();
+         }
+         return false;
+      }
+
+      ///<summary> Using Binary Search algorithm for guessing number </summary>
+      static void GuessNumberBinaryMSB_LSB () {
+         do {
+            Write ($"\nIs {bGuess} higher than/lower than/equal to your number (yes/high/low)? ");
+            string prompt = ReadLine ();
+            if (CheckYes (prompt) || prompt is "high") high = bGuess;
+            else low = bGuess;
+            bGuess = (low + high) / 2;
+         } while (high > low);
+      }
+
+      /// <summary> Using Reverse Binary search algorithm to guess number </summary>
+      static void GuessNumberLSB_MSB () {
+         double sum = 0;
+         for (int i = 1; i <= 7; i++) {
+            Write ($"\nIs your number % {Math.Pow (2, i)} < {Math.Pow (2, i - 1)} (y/n)? ");
+            if (ReadKey ().Key == ConsoleKey.N) sum += Math.Pow (2, i - 1);
+         }
+         Write ($"Your number is {sum}");
       }
       #endregion
    }
