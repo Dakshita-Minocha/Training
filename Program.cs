8519dfdba7996166c34fc72ddccf4cfb8d950eb4
Dakshita <dakshitaminocha@metamation.com>
2023-10-10 18:04:07 +0530

    Made changes after referring to Aravind Sir's video

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace Training {
    internal class Program {
       #region Method ----------------------------------------------
       static int size = 8;
-      static List<List<(int, int)>> solutions = new ();
+      static List<int[]> solutions = new ();
+      static int[] positions = new int[size];
 
       static void Main (string[] args) => Menu ();
 
@@ -23,25 +24,32 @@ namespace Training {
          switch (ReadKey (true).Key) {
             case ConsoleKey.D1: EightQ (); break;
             case ConsoleKey.D2: EightQ ("unique"); break;
+            case ConsoleKey.K: Environment.Exit (0); break;
             default: Menu (); break;
          }
       }
 
       /// <summary> Prints 8queen solution </summary>
-      /// <param name="sol"> Inducates the kind of solutions user wants (All/Fundamental) </param>
+      /// <param name="sol"> Indicates the kind of solutions user wants (All/Fundamental) </param>
       static void EightQ (string sol = "All") {
          OutputEncoding = new UnicodeEncoding ();
-         List<(int row, int col)> positions = new ();
-         FindSol (0, positions, sol);
-         for (int count = 0; count < solutions.Count; count++) {
-            WriteLine (count + 1);
+         FindSol (0, sol);
+         int count = 0, numOfSol = solutions.Count, x, y;
+         (x, y) = GetCursorPosition ();
+         while (count != numOfSol) {
+            CursorTop = y; CursorLeft = x;
+            WriteLine ($"Solution {count + 1} of {numOfSol}");
             for (int i = 0; i < size; i++) {
                PrintLine (i);
                for (int j = 0; j < size; j++)
-                  Write ("\u2502 " + (solutions[count].Contains ((i, j)) ? "♕ " : "  "));
-               Write ('\u2502' + "\n");
+                  Write ("│ " + (solutions[count][j] == i ? "♕ " : "  "));
+               WriteLine ("│");
             }
             PrintLine (8);
+            Write (count == numOfSol - 1 ? "\r\n" : "Press [space] to view next solution");
+            if (ReadKey (true).Key == ConsoleKey.Spacebar) {
+               count++; continue;
+            }
          }
       }
 
@@ -49,82 +57,63 @@ namespace Training {
       /// Column no. = constant : we go through each row to see if there's a position that 
       /// satisfies the condition. </summary>
       /// <param name="col"> Column number, starts with column 0. </param>
-      /// <param name="positions"> List of possible safe positions for the 8 queens. </param>
-      static void FindSol (int col, List<(int row, int column)> positions, string sol) {
-         if (positions.Count == 8) {
-            if (sol == "unique") if (RotatedMirror (positions)) return;
-            solutions.Add (positions.ToList ());
-            return;
-         }
-         for (int i = 0; i < size; i++)
-            if (Check (i, col, positions)) {
-               positions.Add ((i, col));
-               FindSol (col + 1, positions, sol);
-               positions.Remove ((i, col));
+      /// <param name="positions"> List of possible safe positions (row numbers for 
+      /// consecutive columns) for the 8queens. </param>
+      /// <param name="solutions"> List of all possible solutions for the 8queens problem. </param>
+      static void FindSol (int col, string sol) {
+         for (positions[col] = 0; positions[col] < size; positions[col]++)
+            if (Check ()) {
+               if (col == size - 1) {
+                  if (sol == "unique") if (Exists (positions.ToArray ())) return;
+                  solutions.Add (positions.ToArray ());
+               } else FindSol (col + 1, sol);
             }
+
+         // Checks whether it is safe to place a queen in a given position.
+         // Returns true/false based on whether current position is safe.
+         bool Check () {
+            for (int x = 0; x < col; x++) {
+               int dx = col - x, dy = Math.Abs (positions[col] - positions[x]);
+               if (dy == 0 || dy == dx) return false;
+            }
+            return true;
+         }
       }
 
-      /// <summary> Checks whether it is safe to place a queen in a given position. </summary>
-      /// <param name="row"> Current row </param>
-      /// <param name="col"> Current column </param>
-      /// <param name="positions"> List of valid positions according to rules </param>
-      /// <returns> Returns true/false based on whether current position is safe. </returns>
-      static bool Check (int row, int col, List<(int row, int column)> positions) {
-         // same row
-         for (int x = 0; x < size; x++)
-            if (positions.Any (c => c.row == row && c.column == x)) return false;
-         // upper left diagonal
-         for (int x = row, y = col; x >= 0 && y >= 0; x--, y--)
-            if (positions.Any (c => c.row == x && c.column == y)) return false;
-         // lower left diagonal
-         for (int x = row, y = col; x < size && y >= 0; x++, y--)
-            if (positions.Any (c => c.row == x && c.column == y)) return false;
-         // if it is safe to place a queen
-         return true;
-      }
-
-      /// <summary> Checks if the 90, 180 or 270 degree rotated version or mirrors of any of them 
-      /// including current solution exist in the solution list. </summary>
+      /// <summary> Checks if the current solution, 90, 180 or 270 degree rotated version or mirrors
+      /// of any of them exist in the solution list. </summary>
       /// <param name="positions"> Current solution for 8queen problem. </param>
       /// <returns> Returns true if solution already exists. </returns>
-      static bool RotatedMirror (List<(int row, int column)> positions) {
-         List<(int row, int column)> rotate90 = new ();
-         List<(int row, int column)> rotate180 = new ();
-         List<(int row, int column)> rotate270 = new ();
-         // 90 degree check
-         foreach (var pos in positions)
-            rotate90.Add ((pos.column, size - 1 - pos.row));
-         // 180 degree check
-         foreach (var pos in rotate90)
-            rotate180.Add ((pos.column, size - 1 - pos.row));
-         // 270 degree check
-         foreach (var pos in rotate180)
-            rotate270.Add ((pos.column, size - 1 - pos.row));
-         // return true if solutions already contains rotated solution, or mirrors of rotated solutions
-         return solutions.Any (sol => rotate90.All (sol.Contains) || rotate180.All (sol.Contains) || rotate270.All (sol.Contains) ||
-                                      Mirror (positions) || Mirror (rotate90) || Mirror (rotate180) || Mirror (rotate270));
+      static bool Exists (int[] positions) {
+         for (int x = 0; x < 4; x++) {
+            positions = Rotate (positions);
+            if (solutions.Any (sol => sol.SequenceEqual (positions) || sol.SequenceEqual (Mirror (positions)))) return true;
+         }
+         return false;
       }
 
-      /// <summary> Checks if the current solution (stored in positions list) has an existing mirror already in list of solutions. </summary>
-      /// <param name="positions"> Current solution for 8queen problem. </param>
-      /// <returns> Returns true, if mirror exists. </returns>
-      static bool Mirror (List<(int row, int column)> positions) {
-         List<(int, int)> hmirror = new ();
-         List<(int, int)> vmirror = new ();
-         foreach (var (row, column) in positions) {
-            hmirror.Add ((row, size - 1 - column)); // Horizontal mirror check
-            vmirror.Add ((size - 1 - row, column)); // Vertical mirror check
-         }
-         return solutions.Any (sol => hmirror.All (sol.Contains) || vmirror.All (sol.Contains));
+      /// <summary> Rotates solution by 90 degrees. Exchanges columns by rows then takes a mirror. </summary>
+      /// <param name="positions"> Current solution list to be rotated. </param>
+      /// <returns> Rotated solution (by 90 degrees) </returns>
+      static int[] Rotate (int[] positions) {
+         int[] temp = new int[size];
+         for (int i = 0; i < size; i++)
+            temp[positions[i]] = size - 1 - i;
+         return temp;
       }
+
+      /// <summary> Mirrors current solution. </summary>
+      /// <param name="positions"> Current solution for 8queen problem. </param>
+      /// <returns> Mirrored solution. </returns>
+      static int[] Mirror (int[] positions) => positions.Select (x => size - 1 - x).ToArray ();
 
       /// <summary> Prints box grid using UNICODE characters. </summary>
       /// <param name="row"> row number </param>
       static void PrintLine (int row) {
          Write (row == 0 ? '┌' : (row == size ? '└' : '├'));
          for (int i = 0; i < size; i++)
-            Write (new string ('─', 3) + (i == size - 1 ? (row == 0 ? '┐' : (row == size ? '┘' : '┤')) :
-                                                                  (row == 0 ? '┬' : (row == size ? '┴' : '┼'))));
+            Write ("───" + (i == size - 1 ? (row == 0 ? '┐' : (row == size ? '┘' : '┤')) :
+                                            (row == 0 ? '┬' : (row == size ? '┴' : '┼'))));
          WriteLine ();
       }
       #endregion
