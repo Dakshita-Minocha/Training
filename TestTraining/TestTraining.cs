870752a2b70c8b5bce48072aa43c21b2c6f5c4b3
Dakshita <dakshitaminocha@metamation.com>
2023-11-06 18:48:23 +0530

    A07: Double.parse - Initial Commit

--- TestTraining/TestTraining.cs
+++ TestTraining/TestTraining.cs
@@ -1,10 +1,68 @@
+// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// TestTraining.cs
+// Program to test double.Parse implementation.
+// ------------------------------------------------------------------------------------------------
+using static Training.DoubleParse;
 namespace TestTraining;
 
 [TestClass]
-public class TestTraining
-{
-    [TestMethod]
-    public void TestMethod1()
-    {
-    }
+public class TestTraining {
+   [TestMethod]
+   public void TestParse () {
+      // Valid cases: 
+      Assert.IsTrue (Parse ("0", out double num));
+      Assert.AreEqual (0, num);
+      Assert.IsTrue (Parse ("7", out num));
+      Assert.AreEqual (7, num);
+      Assert.IsTrue (Parse ("7.1", out num));
+      Assert.AreEqual (7.1, num);
+      Assert.IsTrue (Parse ("08.6", out num));
+      Assert.AreEqual (08.6, num);
+      Assert.IsTrue (Parse ("7897", out num));
+      Assert.AreEqual (7897, num);
+      Assert.IsTrue (Parse ("00990.009", out num));
+      Assert.AreEqual (00990.009, num);
+      Assert.IsTrue (Parse ("0.003", out num));
+      Assert.AreEqual (0.003, num);
+      Assert.IsTrue (Parse ("-7.78", out num));
+      Assert.AreEqual (-7.78, num);
+      Assert.IsTrue (Parse ("-7.78e1", out num));
+      Assert.AreEqual (-77.8, num);
+      Assert.IsTrue (Parse ("-7e2", out num));
+      Assert.AreEqual (-700, num);
+      Assert.IsTrue (Parse ("778e0", out num));
+      Assert.AreEqual (778, num);
+      Assert.IsTrue (Parse ("8e-3", out num));
+      Assert.AreEqual (0.008, num);
+      Assert.IsTrue (Parse ("4e2.3", out num));
+      Assert.AreEqual (798.1049259875515, num);
+      Assert.IsTrue (Parse ("+4e2.3", out num));
+      Assert.AreEqual (798.1049259875515, num);
+      // Invalid Cases:
+      Assert.IsFalse (Parse ("x", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("8e9.-3", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("8-.7e3", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("3.4e4-.3", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("3.4e4+.3", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("-35.-354e1", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("e1", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("1e", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("1jkse", out num));
+      Assert.AreEqual (0, num);
+      Assert.IsFalse (Parse ("1++$6e", out num));
+      Assert.AreEqual (0, num);
+   }
 }
--- Training/Program.cs
+++ Training/Program.cs
@@ -1,33 +1,85 @@
 ﻿// ------------------------------------------------------------------------------------------------
 // Training ~ A training program for new joinees at Metamation, Batch- July 2023.
-// Copyright (c) Metamation India.                                                
+// Copyright (c) Metamation India.
 // ------------------------------------------------------------------
-// Program.cs                                                                     
-// Program on main branch.
+// Program.cs
+// Program to implement double.Parse.
 // ------------------------------------------------------------------------------------------------
-namespace Training {
-   #region class Program --------------------------------------------------------------------------
-   internal class Program {
-      #region Constructors ------------------------------------------
-      #endregion
+using static System.Console;
+namespace Training;
 
-      #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
-      #endregion
-
-      #region Operators ---------------------------------------------
-      #endregion
-
-      #region Implementation ----------------------------------------
-      #endregion
-
-      #region Nested Types ------------------------------------------
-      #endregion
-
-      #region Private Data ------------------------------------------
-      #endregion 
+#region class Program --------------------------------------------------------------------------
+internal class Program {
+   #region Method ------------------------------------------------
+   static void Main (string[] args) {
+      Write ("input:");
+      string input = ReadLine () ?? "";
+      bool isValid = DoubleParse.Parse (input, out double num);
+      WriteLine ("output: " + num + $"\n{(isValid ? "" : "invalid input")}");
    }
    #endregion
 }
+#endregion
+
+#region Class DoubleParse -------------------------------------------------------------------------------
+public class DoubleParse {
+   #region Method ------------------------------------------------
+   /// <summary>Converts string to double.</summary>
+   /// <param name="input">string input</param>
+   /// <param name="num">double number</param>
+   /// <returns>If False is input number is invalid, true otherwise.</returns>
+   public static bool Parse (string input, out double num) {
+      num = 0;
+      if (input != "") {
+         string[] number = input.Split ('e');
+         if (Validate (number[0] ?? "", out bool isNegative)) {
+            num = Digits (number[0], isNegative);
+            if (number.Length > 1)
+               if (Validate (number[1], out isNegative)) {
+                  double exponent = Digits (number[1], isNegative);
+                  num *= Math.Pow (10, exponent);
+               } else {
+                  num = 0;
+                  return false;
+               }
+            return true;
+         }
+      }
+      return false;
+   }
+   #endregion
+
+   #region Implementation ----------------------------------------
+   /// <summary>Returns double of input string</summary>
+   static double Digits (string input, bool isNegative = false) {
+      Dictionary<char, int> numbers = new () { { '0', 0 }, { '1', 1 }, { '2', 2 }, { '3', 3 }, { '4', 4},
+                                               { '5', 5 }, { '6', 6 }, {'7', 7 }, {'8', 8 }, {'9', 9 } };
+      double num = 0;
+      string[] parts = (input[0] is '-' or '+' ? input[1..input.Length] : input).Split ('.');
+      int intPlace = parts[0].Length - 1;
+      foreach (var part in parts)
+         foreach (char c in part)
+            num += numbers[c] * Math.Pow (10, intPlace--);
+      return (isNegative ? -1 : 1) * num;
+   }
+
+   /// <summary>Return true if input is a valid number, as well as its sign.</summary>
+   static bool Validate (string input, out bool negative) {
+      int dotCount = 0, minus = 0, eCount = 0, plusCount = 0;
+      negative = false;
+      if (input == "") return false;
+      foreach (var ch in input)
+         if (!char.IsDigit (ch))
+            switch (ch) {
+               case '.': dotCount++; break;
+               case 'e': eCount++; break;
+               case '-': minus++; break;
+               case '+': plusCount++; break;
+               default: return false;
+            }
+      negative = minus % 2 == 1;
+      return dotCount <= 1 && eCount == 0 && !input[1..input.Length].Any (x => x is '+' or '-');
+   }
+   #endregion
+}
+#endregion
