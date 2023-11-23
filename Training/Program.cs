952eb45bd80adc6bf59e76b8d09c62325bc46a00
Dakshita <dakshitaminocha@metamation.com>
2023-11-23 17:49:17 +0530

    Adding Test for Double ended queue, refactoring Program.cs

--- Training/Program.cs
+++ Training/Program.cs
@@ -14,52 +14,21 @@ internal class Program {
       DoubleQueue<int> q = new ();
       Random random = new ();
       for (int i = 0; i < 50; i++) {
-         int r = random.Next (0, 3);
+         int r = random.Next (0, 4);
          try {
             switch (r) {
                case 0: WriteLine ($"Back EnQ: {i}"); q.BackEnqueue (i); break;
                case 1: WriteLine ($"Fron EnQ: {i}"); q.FrontEnqueue (i); break;
-               case 2: WriteLine ($"Fron DeQ: {i}"); q.FrontDequeue (); break;
-               case 3: WriteLine ($"Back DeQ: {i}"); q.BackDequeue (); break;
+               case 2: WriteLine ($"Fron DeQ: {q.FrontDequeue ()}"); break;
+               case 3: WriteLine ($"Back DeQ: {q.BackDequeue ()}"); break;
             }
          } catch (Exception e) { WriteLine (e.Message); }
          WriteLine (q);
       }
-      //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-      //DoubleQueue<int> q = new ();
-      //WriteLine (q.IsEmpty);
-      //foreach (var num in nums[0..4]) {
-      //   q.BackEnqueue (num);
-      //   WriteLine ($"Back Enqueue: {num}");
-      //}
-      //WriteLine (q);
-      //WriteLine ($"FrontDeQ:{q.FrontDequeue ()}");
-      //WriteLine ($"FrontEnQ: -1");
-      //q.FrontEnqueue (-1);
-      //WriteLine (q);
-      //WriteLine ($"Back Enqueue: {nums[4]}");
-      //q.BackEnqueue (nums[4]);
-      //WriteLine (q);
-      //WriteLine ($"Back Enqueue: {nums[5]}");
-      //q.BackEnqueue (nums[5]);
-      //WriteLine (q);
-      //WriteLine ($"FrontEnQ: -2");
-      //q.FrontEnqueue (-2);
-      //WriteLine (q);
-      //WriteLine ($"FrontEnQ: -3");
-      //q.FrontEnqueue (-3);
-      //WriteLine (q);
-      //WriteLine ($"Back Enqueue: {nums[6]}");
-      //q.BackEnqueue (nums[6]);
-      //WriteLine (q);
-      //WriteLine ($"BackDeQ: {q.BackDequeue ()}");
-      //WriteLine (q);
-      //WriteLine ($"BackDeQ: {q.BackDequeue ()}");
-      //WriteLine (q);
-      //WriteLine ($"Count: {q.Count}");
    }
 }
 #endregion
+
 #region class DoubleQueue<T> ----------------------------------------------------------------------
 public class DoubleQueue<T> {
    #region Constructors ---------------------------------------------
@@ -80,14 +49,13 @@ public class DoubleQueue<T> {
    /// <exception cref="InvalidOperationException">If queue is Empty.</exception>
    public T BackDequeue () {
       if (IsEmpty) throw new InvalidOperationException ("Empty Queue.");
-      mEnd = mEnd == 0 ? Capacity - 1 : (mEnd - 1) % Capacity;
-      T ele = mArray[mEnd];
+      mEnd = (Capacity + mEnd - 1) % Capacity;
       Count--;
-      return ele;
+      return mArray[mEnd];
    }
 
    /// <summary>Enqueues element at back-end of queue.</summary>
-   /// <param name="value">T value to be enqueued.</param>
+   /// <param name="value">Value to be enqueued.</param>
    public void BackEnqueue (T value) {
       if (Count == Capacity) Resize (2);
       mArray[mEnd] = value;
@@ -96,7 +64,7 @@ public class DoubleQueue<T> {
    }
 
    /// <summary>Dequeues element from Front-end of queue.</summary>
-   /// <returns>T element</returns>
+   /// <returns>Dequeued element</returns>
    /// <exception cref="InvalidOperationException">If queue is empty.</exception>
    public T FrontDequeue () {
       if (IsEmpty) throw new InvalidOperationException ("Empty Queue.");
@@ -107,15 +75,15 @@ public class DoubleQueue<T> {
    }
 
    /// <summary>Enqueues element to start of queue.</summary>
-   /// <param name="value">T value to be enqueued.</param>
-
+   /// <param name="value">Value to be enqueued.</param>
    public void FrontEnqueue (T value) {
       if (Count == Capacity) Resize (2);
-      mStart = mStart == 0 ? Capacity - 1: (mStart - 1) % Capacity;
+      mStart = (Capacity + mStart - 1) % Capacity;
       mArray[mStart] = value;
       Count++;
    }
 
+   /// <summary>Returns string of all elements in queue separated by ' '.</summary>
    public override string ToString () {
       string s = "";
       int start = mStart;
@@ -129,21 +97,18 @@ public class DoubleQueue<T> {
 
    #region Implementation -------------------------------------------
    /// <summary>Resizes array to n times size.
-   /// Elements are copied into new array from index Capacity/4 to index Capacity - Capacity/4 
-   /// to leave space for enqueue operations. Queue is resized everytime either front of queue
+   /// Elements are copied into new array from index Capacity/4 to index Capacity - Capacity/4
+   /// to leave space for enqueue operations. Queue is resized every time either front of queue
    /// or back of queue runs out of space.</summary>
-   void Resize (double n) {
-      T[] temp = new T[Capacity * 2];
-      int i = 0, tIn = Capacity / 2;
-      Capacity *= 2;
-      // 
+   void Resize (int n) {
+      T[] temp = new T[Capacity * n];
+      int i = 0, tIn = Capacity / n;
+      Capacity *= n;
       while (i < Count) {
          temp[tIn] = mArray[(mStart + i++) % Count];
          tIn = (tIn + 1) % Capacity;
       }
-      mArray = temp;
-      mStart = Capacity / 4;
-      mEnd = Capacity - Capacity / 4;
+      (mArray, mStart, mEnd) = (temp, Capacity / 4, Capacity - Capacity / 4);
    }
    #endregion
 
