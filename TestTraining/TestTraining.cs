ae24c69560ee13be838c590a147c4af4495578a0
Dakshita Minocha <shitaminocha@outlook.com>
2023-11-23 12:26:44 +0530

    Implementing FrontEnqueue, Initial commit for test

--- TestTraining/TestTraining.cs
+++ TestTraining/TestTraining.cs
@@ -5,13 +5,28 @@
 // TestTraining.cs
 // TestProgram on main branch.
 // ------------------------------------------------------------------------------------------------
+using Training;
+using static System.Console;
 namespace TestTraining;
 
 [TestClass]
-public class TestTraining
-{
-    [TestMethod]
-    public void TestMethod1()
-    {
-    }
+public class TestTraining {
+   [TestMethod]
+   public void DoubleQueue () {
+      DoubleQueue<int> q = new ();
+
+      Random random = new ();
+      for (int i = 0; i < 50; i++) {
+         int r = random.Next (0, 4);
+         try {
+            switch (r) {
+               case 0: WriteLine ($"Back EnQ: {i}"); q.BackEnqueue (i); break;
+               case 1: WriteLine ($"Fron EnQ: {i}"); q.FrontEnqueue (i); break;
+               case 2: WriteLine ($"Fron DeQ: {q.FrontDequeue ()}"); break;
+               case 3: WriteLine ($"Back DeQ: {q.BackDequeue ()}"); break;
+            }
+         } catch (Exception e) { WriteLine (e.Message); }
+         WriteLine (q);
+      }
+   }
 }
--- Training/Program.cs
+++ Training/Program.cs
@@ -11,26 +11,52 @@ namespace Training;
 #region Class Program -----------------------------------------------------------------------------
 internal class Program {
    static void Main (string[] args) {
-      int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
       DoubleQueue<int> q = new ();
-      WriteLine (q.IsEmpty);
-      foreach (var num in nums[0..4]) {
-         q.BackEnqueue (num);
-         WriteLine ($"Back Enqueue: {num}");
+      Random random = new ();
+      for (int i = 0; i < 50; i++) {
+         int r = random.Next (0, 3);
+         try {
+            switch (r) {
+               case 0: WriteLine ($"Back EnQ: {i}"); q.BackEnqueue (i); break;
+               case 1: WriteLine ($"Fron EnQ: {i}"); q.FrontEnqueue (i); break;
+               case 2: WriteLine ($"Fron DeQ: {i}"); q.FrontDequeue (); break;
+               case 3: WriteLine ($"Back DeQ: {i}"); q.BackDequeue (); break;
+            }
+         } catch (Exception e) { WriteLine (e.Message); }
+         WriteLine (q);
       }
-      WriteLine (q);
-      WriteLine ($"FrontDeQ:{q.FrontDequeue ()}");
-      WriteLine ($"Back Enqueue: {nums[4]}");
-      q.BackEnqueue (nums[4]);
-      WriteLine (q);
-      WriteLine ($"Back Enqueue: {nums[5]}");
-      q.BackEnqueue (nums[5]);
-      WriteLine (q);
-      WriteLine ($"BackDeQ: {q.BackDequeue ()}");
-      WriteLine (q);
-      WriteLine ($"BackDeQ: {q.BackDequeue ()}");
-      WriteLine (q);
-      WriteLine ($"Count: {q.Count}");
+      //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+      //DoubleQueue<int> q = new ();
+      //WriteLine (q.IsEmpty);
+      //foreach (var num in nums[0..4]) {
+      //   q.BackEnqueue (num);
+      //   WriteLine ($"Back Enqueue: {num}");
+      //}
+      //WriteLine (q);
+      //WriteLine ($"FrontDeQ:{q.FrontDequeue ()}");
+      //WriteLine ($"FrontEnQ: -1");
+      //q.FrontEnqueue (-1);
+      //WriteLine (q);
+      //WriteLine ($"Back Enqueue: {nums[4]}");
+      //q.BackEnqueue (nums[4]);
+      //WriteLine (q);
+      //WriteLine ($"Back Enqueue: {nums[5]}");
+      //q.BackEnqueue (nums[5]);
+      //WriteLine (q);
+      //WriteLine ($"FrontEnQ: -2");
+      //q.FrontEnqueue (-2);
+      //WriteLine (q);
+      //WriteLine ($"FrontEnQ: -3");
+      //q.FrontEnqueue (-3);
+      //WriteLine (q);
+      //WriteLine ($"Back Enqueue: {nums[6]}");
+      //q.BackEnqueue (nums[6]);
+      //WriteLine (q);
+      //WriteLine ($"BackDeQ: {q.BackDequeue ()}");
+      //WriteLine (q);
+      //WriteLine ($"BackDeQ: {q.BackDequeue ()}");
+      //WriteLine (q);
+      //WriteLine ($"Count: {q.Count}");
    }
 }
 #endregion
@@ -54,7 +80,7 @@ public class DoubleQueue<T> {
    /// <exception cref="InvalidOperationException">If queue is Empty.</exception>
    public T BackDequeue () {
       if (IsEmpty) throw new InvalidOperationException ("Empty Queue.");
-      mEnd = (mEnd - 1) % Capacity;
+      mEnd = mEnd == 0 ? Capacity - 1 : (mEnd - 1) % Capacity;
       T ele = mArray[mEnd];
       Count--;
       return ele;
@@ -83,12 +109,11 @@ public class DoubleQueue<T> {
    /// <summary>Enqueues element to start of queue.</summary>
    /// <param name="value">T value to be enqueued.</param>
 
-   // use mStart to find start of array and insert element at mStart - 1.
    public void FrontEnqueue (T value) {
-      //if (Count == Capacity) Resize (2);
-      //mArray[mWriteS] = value;
-      //mWriteS = (mWriteS + 1) % Capacity;
-      //Count++;
+      if (Count == Capacity) Resize (2);
+      mStart = mStart == 0 ? Capacity - 1: (mStart - 1) % Capacity;
+      mArray[mStart] = value;
+      Count++;
    }
 
    public override string ToString () {
@@ -103,15 +128,22 @@ public class DoubleQueue<T> {
    #endregion
 
    #region Implementation -------------------------------------------
-   /// <summary>Resizes array to n times size.</summary>
+   /// <summary>Resizes array to n times size.
+   /// Elements are copied into new array from index Capacity/4 to index Capacity - Capacity/4 
+   /// to leave space for enqueue operations. Queue is resized everytime either front of queue
+   /// or back of queue runs out of space.</summary>
    void Resize (double n) {
       T[] temp = new T[Capacity * 2];
-      for (int i = 0; i < Capacity; i++)
-         temp[i] = mArray[(mStart + i) % Capacity];
-      mArray = temp;
+      int i = 0, tIn = Capacity / 2;
       Capacity *= 2;
-      mStart = 0;
-      mEnd = Count;
+      // 
+      while (i < Count) {
+         temp[tIn] = mArray[(mStart + i++) % Count];
+         tIn = (tIn + 1) % Capacity;
+      }
+      mArray = temp;
+      mStart = Capacity / 4;
+      mEnd = Capacity - Capacity / 4;
    }
    #endregion
 
