d9b9d1c17b69d8bc5298d51eeaf20ac18c87cbee
Dakshita <dakshitaminocha@metamation.com>
2023-12-14 12:23:11 +0530

    Changes as per PR#44

--- Training/Program.cs
+++ Training/Program.cs
@@ -14,13 +14,13 @@ internal class Program {
 
    static void Anagrams () {
       string[] words = File.ReadAllLines ("C:/etc/words.txt");
-      var anagrams = words.GroupBy (word => string.Join ("", word.OrderBy (c => c)))
+      var anagrams = words.GroupBy (word => string.Join ("", word.Order ()))
                           .Where (group => group.Count () > 1)
-                          .Select (group => group.OrderBy (word => word).ToArray ());
+                          .Select (group => group.Order ().ToArray ()).OrderByDescending (group => group.Length);
 
       var anaFile = File.Create ("C:/etc/Anagrams.txt");
       using (var newfile = new StreamWriter (anaFile))
-         foreach (var list in anagrams.OrderByDescending (list => list.Length))
+         foreach (var list in anagrams)
             newfile.WriteLine ($"{list.Length} {string.Join (" ", list)}");
       anaFile.Close ();
    }
