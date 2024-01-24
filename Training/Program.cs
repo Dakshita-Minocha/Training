6a30f08f8f7950332dd21fc341f290920b2a1e3e
Dakshita <dakshitaminocha@metamation.com>
2024-01-24 11:20:33 +0530

    Test: Question 1

--- Training/Program.cs
+++ Training/Program.cs
@@ -5,29 +5,31 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training {
-   #region class Program --------------------------------------------------------------------------
-   internal class Program {
-      #region Constructors ------------------------------------------
-      #endregion
-
-      #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+using static System.Console;
+namespace Training;
+#region class Program --------------------------------------------------------------------------
+internal class Program {
+   #region Method ------------------------------------------------
+   static void Main (string[] args) {
+      string[] gameResults = File.ReadAllLines ("C:/Work/Dakshita/input.txt");
+      int sumOfIDs = 0, maxRed = 12, maxGreen = 13, maxBlue = 14;
+      foreach (string gR in gameResults) {
+         bool isValid = true;
+         string[] gResult = gR.Split (':');
+         int gameID = int.Parse (gResult[0].Remove (0, 5));
+         foreach (string revealedSet in gResult[1].Replace (';', ',').Split (',')) {
+            int num = int.Parse (revealedSet.Trim ().Split (" ")[0]);
+            if (revealedSet.Contains ("red") && num > maxRed
+               || revealedSet.Contains ("green") && num > maxGreen
+               || num > maxBlue) {
+               isValid = false;
+               break;
+            }
+         }
+         if (isValid) sumOfIDs += gameID;
       }
-      #endregion
-
-      #region Operators ---------------------------------------------
-      #endregion
-
-      #region Implementation ----------------------------------------
-      #endregion
-
-      #region Nested Types ------------------------------------------
-      #endregion
-
-      #region Private Data ------------------------------------------
-      #endregion 
+      WriteLine ("Sum of IDs: " + sumOfIDs);
    }
    #endregion
 }
+#endregion
