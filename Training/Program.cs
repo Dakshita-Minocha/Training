7b285f36bc3a7f101c8c2823c3c5282bf7dfef3a
Dakshita <dakshitaminocha@metamation.com>
2024-01-24 11:46:47 +0530

    ComplexNumbers: Test Question 2

--- Training/Program.cs
+++ Training/Program.cs
@@ -5,29 +5,44 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training {
-   #region class Program --------------------------------------------------------------------------
-   internal class Program {
-      #region Constructors ------------------------------------------
-      #endregion
+namespace Training;
+#region class ComplexNumber --------------------------------------------------------------------------
+internal class ComplexNumber {
+   #region Constructors ------------------------------------------
+   public ComplexNumber (int real, int imag)
+      => (mReal, mImag, mNorm) = (real, imag, Math.Sqrt (Math.Pow (Real, 2) + Math.Pow (Imag, 2)));
 
-      #region Method ------------------------------------------------
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
-      #endregion
+   public ComplexNumber (double real, double imag)
+      => (mReal, mImag, mNorm) = (real, imag, Math.Sqrt (Math.Pow (Real, 2) + Math.Pow (Imag, 2)));
+   #endregion
 
-      #region Operators ---------------------------------------------
-      #endregion
+   #region Properties --------------------------------------------
+   public double Real { get => mReal; set => mReal = value; }
+   double mReal;
 
-      #region Implementation ----------------------------------------
-      #endregion
+   public double Imag { get => mImag; set => mImag = value; }
+   double mImag;
 
-      #region Nested Types ------------------------------------------
-      #endregion
+   public double Norm => mNorm;
+   double mNorm;
+   #endregion
 
-      #region Private Data ------------------------------------------
-      #endregion 
-   }
+   #region Method ------------------------------------------------
+   public static ComplexNumber Add (ComplexNumber a, ComplexNumber b)
+      => new ComplexNumber (a.Real + b.Real, a.Imag + b.Imag);
+
+   public static ComplexNumber Subtract (ComplexNumber a, ComplexNumber b)
+      => new ComplexNumber (a.Real - b.Real, b.Imag - a.Imag);
+
+   public override string ToString () => $"{Real} {(Imag > 0 ? "+" : "")}{Imag}i";
+   #endregion
+
+   #region Operators ---------------------------------------------
+   public static ComplexNumber operator + (ComplexNumber left, ComplexNumber right)
+      => Add (left, right);
+
+   public static ComplexNumber operator - (ComplexNumber left, ComplexNumber right)
+      => Subtract (left, right);
    #endregion
 }
+#endregion
